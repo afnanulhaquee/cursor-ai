05939f855ec85b6b31ab93b49a8e9cf81f9e3e21
afnanulhaquee <afnanulhaq4@gmail.com>
2025-05-27 17:20:35 +0500

    Remove unused GeistMono import from theme-provider component

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { GeistMono } from '@vercel/font/geist-mono'
 
 interface ThemeProviderProps {
   children: React.ReactNode
